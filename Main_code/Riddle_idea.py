7a102816c26f9e305bead773b1de0be56ce31328
C1626152 <allenzj@cardiff.ac.uk>
2016-10-17 17:15:25 +0100

    Riddle idea added to be discussed 18/10/16

---
+++ Main_code/Riddle_idea.py
@@ -0,0 +1,106 @@
+from map import rooms
+from items import *
+from gameparser import *
+
+"""
+
+##########
+Riddle One
+##########
+
+first part of the puzzles for the 'Old Green Tree'.
+
+
+Also to be added:
+					-Locked Office
+					-Office Window
+					-Jacket/Safe
+					-Safe code puzzle
+					-Clue for next puzzle
+
+					"""
+
+
+def riddle_OGT(rooms)
+	if room['name'] == 'room_The_Old_Green_Tree':
+		answer == input("I don't have eyes, but once I did see. Once I had thoughts, but now I'm white and empty.")
+		filter_words()
+		remove_punct()
+		normalise_input()
+		if answer == 'Skull':
+			print("Correct")
+		else:
+			print('Think more halloween!')
+			riddle_OGT()
+
+"""
+
+
+##########
+Riddle Two
+##########
+
+Puzzle for 'The Fat Angel' pub.
+
+Also to be added:
+					-Second structure to puzzle
+					-Possible third
+					-clue to third puzzle
+
+
+
+
+
+"""
+
+
+def riddle_TFA
+	if room['name'] == 'room_The_Fat_Angel':
+		print("I am the first on Earth,")
+		print("The second in Heaven,")
+		print("I appear two times in a week,")
+		print("you can only see me once in a year, although I am in the middle to the sea.")
+		answer == input("What am I?")
+		filter_words()
+		remove_punct()
+		normalise_input()
+		if answer == 'e'or 'E':
+			print("Correct")
+		else:
+			print("Read the sentence closely")
+			riddle_TFA()
+
+
+"""
+
+############
+Riddle Three
+############
+
+
+###########
+Puzzle Four
+###########
+
+Still to be decided:
+					-Third puzzle set
+					-Fourth puzzle set
+					-Further clues
+					-Further puzzles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
